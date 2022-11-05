f8b6a667b431cdfdb9d6f3acffef45d2d55bfc12
Miguel Cabral <44329689+Migueljfc@users.noreply.github.com>
2022-11-05 20:28:53 +0000

    Update register.dart

--- lib/pages/registerScreen/register.dart
+++ lib/pages/registerScreen/register.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_const_constructors
 import 'package:cm_project/blocs/profile_bloc/bloc/profile_bloc.dart';
+import 'package:cm_project/pages/registerScreen/utils/background.dart';
 import 'package:cm_project/pages/registerScreen/utils/format_field.dart';
 import 'package:cm_project/pages/registerScreen/utils/validators.dart';
 import 'package:flutter/material.dart';
@@ -16,66 +17,77 @@ class RegisterPage extends StatelessWidget {
     String name = '';
     String email = '';
     String password = '';
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
+    return Scaffold(
+      body: Background(
+        child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Divider(
-                color: Color.fromARGB(255, 202, 225, 229),
-                height: 70,
-                thickness: 3,
+            children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+              child: Text(
+                "REGISTER",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 22, 104, 218),
+                  fontSize: 36
+                ),
+                textAlign: TextAlign.left,
               ),
-              Text(
-                'Registo',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              space(context),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    TextField(
-                      onChanged: (value) {
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                onChanged: (value) {
                         name = value;
                       },
                       cursorColor: Color.fromARGB(255, 255, 255, 255),
                       cursorWidth: 1,
                       decoration: formatTextField(context, 'Nome'),
-                    ),
-                    space(context),
-                    TextField(
-                      onChanged: (value) {
-                        email = value;
+              ),
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                 onChanged: (value) {
+                        name = value;
                       },
                       cursorColor: Color.fromARGB(255, 255, 255, 255),
                       cursorWidth: 1,
                       decoration: formatTextField(context, 'Email'),
-                    ),
-                    space(context),
-                    TextField(
-                      onChanged: (value) {
+              ),
+            ),
+
+           SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                onChanged: (value) {
                         password = value;
                       },
                       cursorColor: Color.fromARGB(255, 255, 255, 255),
                       cursorWidth: 1,
                       decoration: formatTextField(context, 'Password'),
-                    ),
-                  ],
-                ),
+                obscureText: true,
               ),
-              Divider(
-                color: Color.fromARGB(255, 202, 225, 229),
-                height: 70,
-                thickness: 3,
-              ),
-              FloatingActionButton.extended(
-                backgroundColor: Color.fromARGB(25, 202, 240, 248),
-                shape: ContinuousRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: TextButton(
                 onPressed: () {
                   if (!nameValidator(name, context)) {
                     return;
@@ -93,22 +105,41 @@ class RegisterPage extends StatelessWidget {
                     ),
                   );
                 },
-                label: Text(
-                  'Registar',
-                  style: Theme.of(context).textTheme.subtitle1,
+               
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(70.0),
+                    gradient: new LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 48, 128, 239),
+                        Color.fromARGB(255, 0, 23, 104)
+                      ]
+                    )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "Registar",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  ),
                 ),
               ),
+             
             ],
-          ),
         ),
       ),
     );
   }
 
-  Padding space(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(
-          0, 0, 0, MediaQuery.of(context).size.height * 0.03),
-    );
-  }
+  // Padding space(BuildContext context) {
+  //   return Padding(
+  //     padding: EdgeInsets.fromLTRB(
+  //         0, 0, 0, MediaQuery.of(context).size.height * 0.03),
+  //   );
+  // }
 }
+
